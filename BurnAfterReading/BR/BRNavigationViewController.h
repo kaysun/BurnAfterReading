c4f04562ab1fc83981db483c64a4a57b12809f67
sunling <sunling@nssay.com>
2018-01-20 22:44:00 +0800

    add navigation controller

---
+++ BurnAfterReading/BR/BRNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  BRNavigationViewController.h
+//  BurnAfterReading
+//
+//  Created by 孙玲 on 2018/1/20.
+//  Copyright © 2018年 qinqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRNavigationViewController : UINavigationController
+
+@end
---
+++ BurnAfterReading/BR/BRNavigationViewController.m
@@ -0,0 +1,37 @@
+//
+//  BRNavigationViewController.m
+//  BurnAfterReading
+//
+//  Created by 孙玲 on 2018/1/20.
+//  Copyright © 2018年 qinqin. All rights reserved.
+//
+
+#import "BRNavigationViewController.h"
+
+@interface BRNavigationViewController ()
+
+@end
+
+@implementation BRNavigationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
